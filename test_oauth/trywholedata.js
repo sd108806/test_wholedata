9031761582616052236ebff230dc3835cfa05309
sd108806 <sd108806@ohio.edu>
2014-11-26 18:51:00 -0500

    for whole data

--- test_oauth/trywholedata.js
+++ test_oauth/trywholedata.js
@@ -2,10 +2,17 @@
 
 function get_wholedata(input_path,start, limit,consumer_key_input,consumer_secrete_input )
 {
+
+// first 50 or 49
 document.write("i am here1 ");
 var obj_changesets=get_changesets(input_path,start, limit,consumer_key_input,consumer_secrete_input );
 var total_count=obj_changesets.count;
-var total_times=total_count/49+1;    // total number of featching data
+
+var total_times;
+if(total_count/(limit-1)!=0)
+ total_times=total_count/(limit-1)+1;    // total number of featching data  instore 49 commits to the array commit
+else
+total_times=total_count/(limit-1);
 
 document.write("i am here2 ");
 
@@ -36,27 +43,56 @@ commit_i++;
 
 document.write('internal commit.length   ' + commit.length + '<br/>');
 
-/*
-var start0= obj_changesets.changesets[0].node;
+
+
+// middle  part
+
 
 if(total_times>1)
 {
-for(var i=0; i< (total_times-1); i++)
+for(var i=0; i< (total_times-2); i++)
 {
+var start0= obj_changesets.changesets[0].node;
+
 var obj_changesets=get_changesets(input_path,start0, limit,consumer_key_input,consumer_secrete_input );
 
-var k=obj_changesets.length;
-k--;
-k--;
-for (var i=0;i< (obj_changesets.length-1); i++)
-{commit[commit_i]=obj_changesets.changesets[k];
-k--;
+for(var j=(limit-1); j>0;j--)
+{
+commit[commit_i]=obj_changesets.changesets[j];
 commit_i++;
-}
 }
 
 }
-*/
+}
+
+// final 50 or 49 commmits
+if(total_times>=3)
+{
+var start0= obj_changesets.changesets[0].node;
+var obj_changesets=get_changesets(input_path,start0, limit,consumer_key_input,consumer_secrete_input );
+
+var k2;
+if(obj_changesets.changesets.length==50)
+k2=49;
+else
+k2=obj_changesets.changesets.length;
+
+
+k2--;
+
+document.write("obj_changesets.changesets.length "+obj_changesets.changesets.length+ "<br/>");
+document.write("k2 "+k2+ "<br/>");
+
+
+for (var i=0;i< obj_changesets.changesets.length; i++)
+{commit[commit_i]=obj_changesets.changesets[k2];
+k2--;
+commit_i++;
+}
+
+}
+
+document.write("commit.length "+commit.length+ "<br/>");
 
 return commit;
 
